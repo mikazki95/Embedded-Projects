e9c222c1c36969842143646be1e44787c847e991
Tonatiuh Velzquez Rojas <tonatiuh_vr95@hotmail.com>
2025-10-13 12:34:40 -0600

    ok_2
    falta documentacion, aun falla a altas frecuencias, primero terminar y luego ver si da tiempo

--- Testing_Scripts/nxp_simtemp_challenge/Sensor/driver/.nxp_simtemp.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/tona/Escritorio/Sensor/driver/nxp_simtemp.ko := ld -r -m elf_i386  --build-id  -T ./scripts/module-common.lds -o /home/tona/Escritorio/Sensor/driver/nxp_simtemp.ko /home/tona/Escritorio/Sensor/driver/nxp_simtemp.o /home/tona/Escritorio/Sensor/driver/nxp_simtemp.mod.o;  true
--- Testing_Scripts/nxp_simtemp_challenge/Sensor/driver/.nxp_simtemp.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/tona/Escritorio/Sensor/driver/nxp_simtemp.mod := { echo  /home/tona/Escritorio/Sensor/driver/nxp_simtemp.o; echo; } > /home/tona/Escritorio/Sensor/driver/nxp_simtemp.mod
