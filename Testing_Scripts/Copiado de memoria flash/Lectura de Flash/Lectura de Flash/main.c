0e5a551870f96c85d863fbac91a5e4c9bb137c98
Tonatiuh Velzquez Rojas <tonatiuh_vr95@hotmail.com>
2025-11-14 13:23:47 -0600

    Comandos basicos implementados

--- Testing_Scripts/Copiado de memoria flash/Lectura de Flash/Lectura de Flash/main.c
+++ Testing_Scripts/Copiado de memoria flash/Lectura de Flash/Lectura de Flash/main.c
@@ -14,12 +14,14 @@
 
 #include "USART0.h"
 
-char	comando='0';
-char	funcion_RW = '0';
-char	valor_m[10]={0};
-int		size = 32584256;
+char		comando='0';
+char		funcion_RW = '0';
+char		valor_m[15]={0};
+uint32_t	size = 33554432;
+char		texto_info[10]		= {"I:Flash,"};
+char		version[10]	= {"1.0.0"};
 
-ISR (USART_RX_vect)
+ISR(USART0_RX_vect)
 {
 	comando=UDR0;
 	if (comando =='R')
@@ -40,38 +42,30 @@ ISR (USART_RX_vect)
 		send_char('\n');
 		funcion_RW = 'E';
 	}
-	///////////////////////////////////////////debug
 	
 }
 
 int main(void)
 {
     /* Replace with your application code */
+	cli();
+	
+	usart_init();
+
+	UCSR0B =  (1<<RXEN0) | (1<<TXEN0)|(1<<RXCIE0);
+	
+	sei();
+	
+	send_string(texto_info);
+	send_string(version);
+	send_char('\n');
+	
     while (1) 
     {
-		DDRB = 0b00000111;		
-
-		DDRD	= 0b00100000;
-		PORTD	= 0x00;
-		TCCR0A	= 0x23;
-		TCCR0B	= 0x01;
-		TCCR2A	= 0x02;
-		OCR2A	= 230;
-		TCCR2B	= 0x05;
-		TIMSK2	= 0x02;
-		OCR0B=0x01;
-		PORTB = 0x03;//0x02;
-		cli();
-		
-		usart_init();
-
-		UCSR0B =  (1<<RXEN0) | (1<<TXEN0)|(1<<RXCIE0);
-		
-		sei();
 		if (funcion_RW == 'R')
 		{
 			send_string("preparando para leer: ");
-			dtostrf(size,3,0,valor_m);
+			dtostrf(size,10,0,valor_m);
 			send_string(valor_m);
 			send_string("bytes");
 			send_char('\n');
